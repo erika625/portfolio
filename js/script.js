0b2cdabeadeac0e90f7dac26ada66eb887e84eab
hirata erika <biiiiii.uw@gmail.com>
2020-12-17 15:17:04 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@
 
 
 ///// hamberger /////
-(function($) {
+$(function(){
   var $nav   = $('#navArea');
   var $btn   = $('.toggle_btn');
   var $mask  = $('.gnav_mask');
@@ -41,13 +41,64 @@
   $mask.on('click', function() {
     $nav.removeClass( open );
   });
-} )(jQuery);
+});
 
-///// more /////
+var nav_pos = $('.gnav').offset().top;
+var nav_height = $('.gnav').outerHeight();
+$(window).scroll(function () {
+    if ($(this).scrollTop() > nav_pos) {
+        $('body').css('padding-top', nav_height);
+        $('.gnav').addClass('fixed');
+        $('header').addClass('header_bg');
+    } else {
+        $('body').css('padding-top', 0);
+        $('.gnav').removeClass('fixed');
+        $('header').removeClass('header_bg');
+    }
+});
+
 // $(function(){
-//     $(window).on('load scroll', function() {
-//        var winScroll = $(window).scrollTop();
-//        var winHeight = $(window).height();
-//        var scrollPos = winScroll + (winHeight * 0.8);
-//     });
-//  });
+//   var $like_btn_next = $('.btn_next');
+//   var $like_btn_prev = $('.btn_prev');
+//   $like_btn_next.on('click',function(){
+//     if()
+//   })  
+// })
+
+
+
+// swiper
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+	spaceBetween: 0,
+	// initialSlide: 2,
+	loop: true,
+	slidesPerView: 4,
+	breakpoints: {
+		// 768px以上の場合
+		768: {
+		slidesPerView: 1,
+		},
+		// 980px以上の場合
+		980: {
+		slidesPerView: 2,
+		},
+		// 1200px以上の場合
+		1200: {
+		slidesPerView: 3,
+		}
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+  });
